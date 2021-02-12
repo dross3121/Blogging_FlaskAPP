0335cb15feac35a7b4495b150e25616affa33356
Deshawn Ross <71471435+dross3121@users.noreply.github.com>
2021-02-12 12:03:30 -0500

    ...

--- app/models.py
+++ app/models.py
@@ -37,6 +37,17 @@ class User(UserMixin, db.Model):
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
+    def follow(self, user):
+	    if not self.is_following(user):
+	        self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
 	
 
 
